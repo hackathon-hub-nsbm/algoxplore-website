dcd9debaea5b2edfd0769395cb717fbeda763099
Danuja <danujadanuja25@gmail.com>
2024-10-18 12:48:26 +0530

    feat: create about component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import About from "@/components/about";
 import Navbar from "@/components/navbar";
 import RegistrationForm from "@/components/registrationForm";
 import { useState } from "react";
@@ -72,16 +73,7 @@ export default function Home() {
 
           <div className="ml-auto overflow-y-auto z-50 terminal_right">
             {activeTab === "About" &&
-              <div className="lg:pr-20">
-                <p className="text">
-                  AlgoXplore 1.0 is event organized by the []. The event is aimed at providing a platform for students to explore the world of algorithms and data structures. The event will consist of a series of lectures and hands-on sessions on various topics in algorithms and data structures. The event will also include a coding competition where participants can showcase their skills in competitive programming
-                  <span className="blinking_underscore"> _</span>
-                </p>
-                <img className="mt-5 about_image" src="/algoexplore.jpg" alt="AlgoXplore" width={500} height={200} />
-                <p className="pt-5 text">
-                  The event is open to all students who are interested in algorithms and data structures. Participants can register as a team of up to four members. The event will be held on 10th October 2021 at the []. Participants are required to bring their own laptops for the coding competition. The event is free of charge and lunch will be provided. //AI Generated Text.
-                </p>
-              </div>
+              <About />
             }
 
             {activeTab === "Menu 2" && <div>Menu Content 2</div>}
