e240dcb9afbedd5cbfdde0c58512b15647ed797d
Zerone Brianstorm <ometha12@gmail.com>
2024-10-21 17:01:32 +0530

    Minor Ui Tweaks

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import About from "@/components/about";
 import Navbar from "@/components/navbar";
 import RegistrationForm from "@/components/registrationForm";
@@ -6,7 +6,6 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 export default function Home() {
-
   const tabs = ["About", "Menu 2", "Menu 3", "Menu 4", "Register"];
   const [activeTab, setActiveTab] = useState("About");
 
@@ -36,12 +35,12 @@ export default function Home() {
   }, [activeTab]);
 
   useEffect(() => {
-    const crtElement = document.querySelector('.crt_effect');
+    const crtElement = document.querySelector(".crt_effect");
     if (crtElement) {
       const interval = setInterval(() => {
-        crtElement.classList.add('flicker_delay');
+        crtElement.classList.add("flicker_delay");
         setTimeout(() => {
-          crtElement.classList.remove('flicker_delay');
+          crtElement.classList.remove("flicker_delay");
         }, 1900);
       }, 5000);
 
@@ -51,20 +50,18 @@ export default function Home() {
 
   return (
     <main className="main flex items-center justify-center min-h-screen">
-
       <div className="overflow-x-hidden overflow-y-scroll terminal crt_effect">
-
         <Navbar />
 
         <div className="flex flex-col justify-between items-center lg:flex-row lg:items-start terminal_data">
-
           <div className=" w-full pt-9 lg:fixed lg:pt-0 lg:w-2/4">
             <div className="flex gap-2 justify-center items-center lg:flex-col lg:items-start">
               {tabs.map((tab) => (
                 <div
                   key={tab}
                   onClick={() => setActiveTab(tab)}
-                  className={`cursor-pointer menu_item ${activeTab === tab ? "bg-[#38ac38] text-black" : ""}`}>
+                  className={`cursor-pointer menu_item ${activeTab === tab ? "bg-[#38ac38] text-black" : ""}`}
+                >
                   {tab}
                 </div>
               ))}
@@ -72,23 +69,16 @@ export default function Home() {
           </div>
 
           <div className="ml-auto overflow-y-auto z-50 terminal_right">
-            {activeTab === "About" &&
-              <About />
-            }
+            {activeTab === "About" && <About />}
 
             {activeTab === "Menu 2" && <div>Menu Content 2</div>}
             {activeTab === "Menu 3" && <div>Menu Content 3</div>}
             {activeTab === "Menu 4" && <div>Menu Content 4</div>}
 
-            {activeTab === "Register" &&
-              <RegistrationForm />
-            }
+            {activeTab === "Register" && <RegistrationForm />}
           </div>
-
         </div>
-
       </div>
-
     </main>
   );
 }
--- components/registrationForm.tsx
+++ components/registrationForm.tsx
@@ -1,66 +1,134 @@
 export default function RegistrationForm() {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+    const formData = new FormData(event.currentTarget);
 
-        const formData = new FormData(event.currentTarget);
+    try {
+      const result = await fetch(event.currentTarget.action, {
+        method: "POST",
+        body: formData,
+        mode: "no-cors",
+      });
+      if (result.ok || result.status === 0) {
+        window.alert("Registration Successful. Thank you for registering.");
+        window.location.reload();
+      } else {
+        window.alert("Sorry, Something went wrong. Please try again later.");
+      }
+    } catch (error) {
+      window.alert("Sorry, Something went wrong. Please try again later.");
+      console.log(error);
+    }
+  };
 
-        try {
-            const result = await fetch(event.currentTarget.action, {
-                method: 'POST',
-                body: formData,
-                mode: 'no-cors',
-            });
-            if (result.ok || result.status === 0) {
-                window.alert('Registration Successful. Thank you for registering.');
-                window.location.reload();
-            } else {
-                window.alert('Sorry, Something went wrong. Please try again later.');
-            }
-        } catch (error) {
-            window.alert('Sorry, Something went wrong. Please try again later.');
-            console.log(error);
-        }
-    };
-
-    return (
-        <div>
-            <p className="text">Register for the AlgoXplore 1.0</p>
-            <form className="pt-9" action="https://docs.google.com/forms/d/e/1FAIpQLScyu_WYhD3NycnjeIXOBh1YROtu8pX9T6Zp0h7ayHS-DJ91VQ/formResponse" onSubmit={handleSubmit}>
-                <div className="flex flex-col gap-6 lg:flex-row lg:gap-20">
-                    <div className="flex flex-col">
-                        <label htmlFor="teamname">Team Name</label>
-                        <input className="input mt-1" type="text" name="entry.663129380" placeholder="Group X" required />
-                    </div>
-                    <div className="flex flex-col">
-                        <label htmlFor="email">Email</label>
-                        <input className="input mt-1" type="email" name="entry.1438216699" placeholder="example@email.com" required />
-                    </div>
-                    <div className="flex flex-col">
-                        <label htmlFor="mobile">Contact Number</label>
-                        <input className="input mt-1" type="text" name="entry.278976986" placeholder="07X-XXXX-XXX" required />
-                    </div>
-                </div>
-                <div className="flex flex-col gap-6 pt-6 lg:flex-row lg:gap-20 lg:pt-9">
-                    <div className="flex flex-col">
-                        <label htmlFor="university">University</label>
-                        <input className="input mt-1" type="text" name="entry.88928883" placeholder="NSBM" required />
-                    </div>
-                    <div className="flex flex-col">
-                        <label htmlFor="foodPreference">Food Preference <span className="text-xs">(Veg or Non-veg)</span></label>
-                        <input className="input3 mt-1" type="text" name="entry.1489052153" placeholder="Veg or Non-Veg" required />
-                    </div>
-                </div>
-                <div className="flex flex-col pt-6 lg:pt-9">
-                    <label htmlFor="member">Team Members Names <span className="text-xs">(To be printed on certificates)</span></label>
-                    <input className="input2 mt-1" type="text" name="entry.808958187" placeholder="Member 1" required />
-                    <input className="input2 mt-1" type="text" name="entry.1178906780" placeholder="Member 2" required />
-                    <input className="input2 mt-1" type="text" name="entry.26362077" placeholder="Member 3" />
-                    <input className="input2 mt-1" type="text" name="entry.1779595281" placeholder="Member 4" />
-                </div>
-                <button className="btn mt-9 border p-2 text-sm border-[#38ac38] hover:bg-[#38ac38] hover:text-black" type="submit"> &gt;&gt; Register</button>
-            </form>
-            <p className="text-xs pt-9"><sup>*</sup>Minimum 2 members are required to participate in the event.</p>
+  return (
+    <div>
+      <p className="text underscan-l">Register for the AlgoXplore 1.0</p>
+      <form
+        className="pt-9"
+        action="https://docs.google.com/forms/d/e/1FAIpQLScyu_WYhD3NycnjeIXOBh1YROtu8pX9T6Zp0h7ayHS-DJ91VQ/formResponse"
+        onSubmit={handleSubmit}
+      >
+        <div className="flex flex-col gap-6 lg:flex-row lg:gap-20">
+          <div className="flex flex-col">
+            <label htmlFor="teamname">Team Name</label>
+            <input
+              className="input mt-1"
+              type="text"
+              name="entry.663129380"
+              placeholder="Group X"
+              required
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="email">Email</label>
+            <input
+              className="input mt-1"
+              type="email"
+              name="entry.1438216699"
+              placeholder="example@email.com"
+              required
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="mobile">Contact Number</label>
+            <input
+              className="input mt-1"
+              type="text"
+              name="entry.278976986"
+              placeholder="07X-XXXX-XXX"
+              required
+            />
+          </div>
         </div>
-    )
+        <div className="flex flex-col gap-6 pt-6 lg:flex-row lg:gap-20 lg:pt-9">
+          <div className="flex flex-col">
+            <label htmlFor="university">University</label>
+            <input
+              className="input mt-1"
+              type="text"
+              name="entry.88928883"
+              placeholder="NSBM"
+              required
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="foodPreference">
+              Food Preference <span className="text-xs">(Veg or Non-veg)</span>
+            </label>
+            <input
+              className="input3 mt-1"
+              type="text"
+              name="entry.1489052153"
+              placeholder="Veg or Non-Veg"
+              required
+            />
+          </div>
+        </div>
+        <div className="flex flex-col pt-6 lg:pt-9">
+          <label htmlFor="member">
+            Team Members Names{" "}
+            <span className="text-xs">(To be printed on certificates)</span>
+          </label>
+          <input
+            className="input2 mt-1"
+            type="text"
+            name="entry.808958187"
+            placeholder="Member 1"
+            required
+          />
+          <input
+            className="input2 mt-1"
+            type="text"
+            name="entry.1178906780"
+            placeholder="Member 2"
+            required
+          />
+          <input
+            className="input2 mt-1"
+            type="text"
+            name="entry.26362077"
+            placeholder="Member 3"
+          />
+          <input
+            className="input2 mt-1"
+            type="text"
+            name="entry.1779595281"
+            placeholder="Member 4"
+          />
+        </div>
+        <button
+          className="btn mt-9 border p-2 text-sm border-[#38ac38] hover:bg-[#38ac38] hover:text-black"
+          type="submit"
+        >
+          {" "}
+          &gt;&gt; Register
+        </button>
+      </form>
+      <p className="text-xs pt-9">
+        <sup>*</sup>Minimum 2 members are required to participate in the event.
+      </p>
+    </div>
+  );
 }
